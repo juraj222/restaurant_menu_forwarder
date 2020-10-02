7e60cd7df38fba3fa6d7f73080bc4447351fb6cd
juraj.vrabec <juraj.vrabec@itera.no>
2020-10-02 15:23:23 +0200

    init springboot

---
+++ src/main/java/com/foodmenus/foodmenus/FoodmenusApplication.java
@@ -0,0 +1,13 @@
+package com.foodmenus.foodmenus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmenusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmenusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodmenus/foodmenus/FoodmenusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodmenus.foodmenus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmenusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
